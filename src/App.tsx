4e9082dd27d15d19f681a97137cca33f57e2cbf6
datsfilipe <datsfilipe@gmail.com>
2021-08-31 05:26:45 -0300

    update app and index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import theme from '../src/styles/themes/theme';
+import GlobalStyle from '../src/styles/global';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
@@ -12,12 +16,15 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/rooms/new" component={NewRoom} />
-          <Route path="/rooms/:id" component={Room} />
-          <Route path="/admin/rooms/:id" component={AdminRoom} />
-        </Switch>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/rooms/new" component={NewRoom} />
+            <Route path="/rooms/:id" component={Room} />
+            <Route path="/admin/rooms/:id" component={AdminRoom} />
+          </Switch>
+        </ThemeProvider>
       </AuthContextProvider>
     </BrowserRouter>
   );
