2761754d26001daa6bae977958455d1b589a839e
datsfilipe <datsfilipe@gmail.com>
2021-09-01 08:48:06 -0300

    changes on app file cause theme creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 
-import theme from '../src/styles/themes/theme';
 import GlobalStyle from '../src/styles/global';
+import { ThemeContextProvider } from './contexts/ThemeContext';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
@@ -11,20 +11,26 @@ import { Room } from './pages/Room';
 import { AuthContextProvider } from './contexts/AuthContext';
 import { AdminRoom } from './pages/AdminRoom';
 
+
+
 function App() {
 
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Switch>
-            <Route path="/" exact component={Home}/>
-            <Route path="/rooms/new" component={NewRoom} />
-            <Route path="/rooms/:id" component={Room} />
-            <Route path="/admin/rooms/:id" component={AdminRoom} />
-          </Switch>
-        </ThemeProvider>
+        <ThemeContextProvider>
+          <ThemeProvider theme={
+            (props: DefaultTheme) => props}
+          >
+            <GlobalStyle />
+            <Switch>
+              <Route path="/" exact component={Home}/>
+              <Route path="/rooms/new" component={NewRoom} />
+              <Route path="/rooms/:id" component={Room} />
+              <Route path="/admin/rooms/:id" component={AdminRoom} />
+            </Switch>
+          </ThemeProvider>
+        </ThemeContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
   );
