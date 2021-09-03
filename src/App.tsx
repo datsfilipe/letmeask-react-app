155c0f9094748d4018e489751dc081322717a11f
datsfilipe <datsfilipe@gmail.com>
2021-09-03 12:11:09 -0300

    update themes, switch theme working now

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import GlobalStyle from '../src/styles/global';
 import { ThemeContextProvider } from './contexts/ThemeContext';
-import { DefaultTheme, ThemeProvider } from 'styled-components';
-
+import { Theme } from './components/Theme/index';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
@@ -11,25 +9,19 @@ import { Room } from './pages/Room';
 import { AuthContextProvider } from './contexts/AuthContext';
 import { AdminRoom } from './pages/AdminRoom';
 
-
-
 function App() {
-
   return (
     <BrowserRouter>
       <AuthContextProvider>
         <ThemeContextProvider>
-          <ThemeProvider theme={
-            (props: DefaultTheme) => props}
-          >
-            <GlobalStyle />
+          <Theme>
             <Switch>
               <Route path="/" exact component={Home}/>
               <Route path="/rooms/new" component={NewRoom} />
               <Route path="/rooms/:id" component={Room} />
               <Route path="/admin/rooms/:id" component={AdminRoom} />
             </Switch>
-          </ThemeProvider>
+          </Theme>
         </ThemeContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
