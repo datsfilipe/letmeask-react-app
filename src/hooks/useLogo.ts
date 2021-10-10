8eb77e95d2842f266c11b7764b25b3cfccec2cad
datsfilipe <datsfilipe@gmail.com>
2021-10-09 22:19:52 -0300

    delete useLogo

--- src/hooks/useLogo.ts
+++
@@ -1,9 +0,0 @@
-import logoLight from '../assets/images/logo-light.svg';
-import logoDark from '../assets/images/logo-dark.svg';
-import { useState } from 'react';
-
-export function useLogo() {
-  const [logo, setLogo] = useState(logoLight);
-
-  return { logo, setLogo, logoLight, logoDark };
-}
